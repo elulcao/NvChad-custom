c2ba7e09cd920f3618662a212d48955bfa7543e2
Daniel Carvallo <elulcao@icloud.com>
2023-05-24 11:31:43 -0600

    Copilot update

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -88,4 +88,15 @@ M.nvimtree = {
   },
 }
 
+M.copilot = {
+-- Possible configurable fields can be found on:
+-- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+  suggestion = {
+    enable = false,
+  },
+  panel = {
+    enable = false,
+  },
+}
+
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -73,10 +73,36 @@ local plugins = {
   },
   {
     "m4xshen/autoclose.nvim",
-      event = "BufEnter",
-      config = function()
+    event = "BufEnter",
+    config = function()
         require("autoclose").setup()
-      end,
+    end,
+  },
+  {
+	"zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+  {
+	"hrsh7th/nvim-cmp",
+    dependencies = {
+    	{
+		    "zbirenbaum/copilot-cmp",
+            config = function()
+			require("copilot_cmp").setup()
+			end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
   },
 }
 
