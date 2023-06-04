085625390eca052b96079d7b8313232416c4064e
Daniel Carvallo <elulcao@icloud.com>
2023-06-04 15:14:31 -0600

    Use prettier

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -6,7 +6,7 @@ local opts = {
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.stylua,
     -- null_ls.builtins.formatting.golines,
   },
