f1db0cfe08100385b9f10ff207e4fdc9029e832e
Daniel Carvallo <elulcao@icloud.com>
2023-11-23 15:22:42 -0600

    Update nvimtree

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -93,13 +93,20 @@ M.nvimtree = {
 	filters = {
 		dotfiles = false,
 	},
+	view = {
+		width = 40,
+	},
 	renderer = {
+		group_empty = true,
 		highlight_git = true,
 		icons = {
 			show = {
 				git = true,
 			},
 		},
+	},
+	sort = {
+		sorter = "case_sensitive",
 	},
 }
 
