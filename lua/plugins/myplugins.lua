537c2158477decb121146223c70da743207dcba7
elulcao <elulcao@icloud.com>
2025-02-16 11:38:24 -0600

    remove lazy lock

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -162,12 +162,24 @@ local plugins = {
 	{
 		"nvzone/menu",
 		lazy = true,
-    config = function()
-      require("menu").setup({
-        position = "center",
-        border = "rounded",
-      })
-    end,
+		config = function()
+			require("menu").setup({
+				position = "center",
+				border = "rounded",
+			})
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
 	},
   { 
     "nvim-neotest/nvim-nio" 
